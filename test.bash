ed9c0b4018cf438acf39c08efecf9a38972b79bd
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2020-11-01 02:10:59 +0000

    bash files

---
+++ test.bash
@@ -0,0 +1,3 @@
+for d in 0.0 0.9 0.99 0.999 0.9999 0.99999 0.999999; do
+    python open_lth.py lottery --default_hparams=cifar_vgg_16 --levels=3 --dataset_name=cifar100 --pruning_layers_to_ignore=None --pruning_fraction=$d  --training_steps=2ep  --milestone_steps=1ep --nesterov_momentum=0.9
+done
